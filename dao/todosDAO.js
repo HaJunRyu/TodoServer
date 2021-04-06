3c89c5aec7430f89f5b24f034414d7b13b1a5fb2
HaJunRyu <hjr4557@naver.com>
2021-04-06 16:25:15 +0900

    initial commit: TodoServer 초기 구성 new file:   .gitignore new file:   dao/todosDAO.js new file:   package-lock.json new file:   package.json new file:   server.js

---
+++ dao/todosDAO.js
@@ -0,0 +1,92 @@
+const mysql = require('mysql2/promise');
+const dbconfig = require('../config/database.js');
+const pool = mysql.createPool(dbconfig);
+
+exports.getTodos = async () => {
+  try {
+    const connection = await pool.getConnection(async conn => conn);
+    try {
+      const rows = await connection.query('SELECT * FROM Todo');
+      connection.release();
+      return rows;
+    } catch (err) {
+      connection.release();
+      return 'Query Error';
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.createTodo = async ({ title, completed = false } = {}) => {
+  try {
+    const connection = await pool.getConnection(async conn => conn);
+    try {
+      const [rows] = await connection.query('INSERT INTO Todo(title, completed) values (?, ?)', [
+        title,
+        completed
+      ]);
+      connection.release();
+      return rows;
+    } catch (err) {
+      connection.release();
+      return 'Query Error';
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.removeTodo = async ({ idTodo }) => {
+  try {
+    const connection = await pool.getConnection(async conn => conn);
+    try {
+      const [rows] = await connection.query('DELETE FROM Todo WHERE idTodo = (?)', [idTodo]);
+      connection.release();
+      return rows;
+    } catch (err) {
+      connection.release();
+      return 'Query Error';
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.updateTodo = async ({ id, title }) => {
+  try {
+    const connection = await pool.getConnection(async conn => conn);
+    try {
+      const [rows] = await connection.query('UPDATE Todo SET title = (?) WHERE idTodo = (?)', [
+        title,
+        id
+      ]);
+      connection.release();
+      return rows;
+    } catch (err) {
+      connection.release();
+      return 'Query Error';
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.toggleTodo = async ({ id, completed }) => {
+  try {
+    const connection = await pool.getConnection(async conn => conn);
+    try {
+      const [rows] = await connection.query('UPDATE Todo SET completed = (?) WHERE idTodo = (?)', [
+        !completed,
+        id
+      ]);
+      connection.release();
+      return rows;
+    } catch (err) {
+      connection.release();
+      return 'Query Error';
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+const { getTodos, createTodo, removeTodo, updateTodo, toggleTodo } = require('./dao/todosDAO');
+
+app.post('/add_todo', async (req, res) => {
+  const createResult = await createTodo(req.body);
+
+  if (!createResult) res.send(false);
+  else {
+    // 요청받은 이메일에 해당하는 유저가 있을때 유저 객체 응답
+    const { insertId } = createResult;
+    const responseObj = { id: insertId, title: req.body.title, completed: false };
+    res.send(responseObj);
+  }
+});
+
+app.get('/fetch_todo', async (req, res) => {
+  const [todos] = await getTodos();
+  res.send(todos);
+});
+
+app.patch('/update_todo', async (req, res) => {
+  const updateResult = await updateTodo(req.body);
+  res.send(updateResult.affectedRows ? req.body : false);
+});
+
+app.patch('/toggle_todo', async (req, res) => {
+  const toggleResult = await toggleTodo(req.body);
+  const completed = !req.body.completed;
+  res.send(toggleResult.affectedRows ? { ...req.body, completed } : false);
+});
+
+app.delete('/remove_todo', (req, res) => {
+  const removeResult = removeTodo(req.body.id);
+  res.send(removeResult ? true : false);
+});
+
+app.set('port', process.env.PORT || 3100);
+
+app.listen(app.get('port'), () => {
+  console.log('Express server listening on port ' + app.get('port'));
+});
